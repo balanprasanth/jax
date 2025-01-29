9d152b5fa9b4eb5ebcf1c799b0ce9340500373a3
balanprasanth <112931254+balanprasanth@users.noreply.github.com>
2025-01-29 10:44:39 +0530

    Update csat.js

--- .github/workflows/scripts/csat.js
+++ .github/workflows/scripts/csat.js
@@ -39,24 +39,19 @@ module.exports = async ({ github, context }) => {
                 baseUrl = CONSTANT_VALUES.MODULE.CSAT.MEDIA_PIPE_BASE_URL;
             else
                 baseUrl = CONSTANT_VALUES.MODULE.CSAT.BASE_URL;
-
-            const yesCsat = `<a href="${baseUrl + CONSTANT_VALUES.MODULE.CSAT.SATISFACTION_PARAM +
-                CONSTANT_VALUES.MODULE.CSAT.YES +
-                CONSTANT_VALUES.MODULE.CSAT.ISSUEID_PARAM + issue}"> ${CONSTANT_VALUES.MODULE.CSAT.YES}</a>`;
-
-            const noCsat = `<a href="${baseUrl + CONSTANT_VALUES.MODULE.CSAT.SATISFACTION_PARAM +
-                CONSTANT_VALUES.MODULE.CSAT.NO +
-                CONSTANT_VALUES.MODULE.CSAT.ISSUEID_PARAM + issue}"> ${CONSTANT_VALUES.MODULE.CSAT.NO}</a>`;
              
-            const params_1 = new URLSearchParams({ [CONSTANT_VALUES.MODULE.CSAT.SATISFACTION_PARAM.toString()]: CONSTANT_VALUES.MODULE.CSAT.YES.toString(), [CONSTANT_VALUES.MODULE.CSAT.ISSUEID_PARAM.toString()]: issue});
-            const link_1 = `${baseUrl}?${params_1.toString()}`;
-            const params_2 = new URLSearchParams({
+            const yesCsatparams = new URLSearchParams({ 
+                 [CONSTANT_VALUES.MODULE.CSAT.SATISFACTION_PARAM.toString()]: CONSTANT_VALUES.MODULE.CSAT.YES.toString(), 
+                 [CONSTANT_VALUES.MODULE.CSAT.ISSUEID_PARAM.toString()]: issue
+            });
+            const yesCsatlink = `${baseUrl}?${yesCsatparams.toString()}`;
+            const noCsatparams = new URLSearchParams({
                    [CONSTANT_VALUES.MODULE.CSAT.SATISFACTION_PARAM.toString()]: CONSTANT_VALUES.MODULE.CSAT.NO.toString(),
                    [CONSTANT_VALUES.MODULE.CSAT.ISSUEID_PARAM.toString()]: issue
             });
-            const link_2 = `${baseUrl}?${params_2.toString()}`; 
-            const comment = CONSTANT_VALUES.MODULE.CSAT.MSG + '\n' + `[${CONSTANT_VALUES.MODULE.CSAT.YES.toString()}](${link_1})` + '\n' +
-                `[${CONSTANT_VALUES.MODULE.CSAT.NO.toString()}](${link_2})` + '\n';
+            const noCsatlink = `${baseUrl}?${noCsatparams.toString()}`; 
+            const comment = CONSTANT_VALUES.MODULE.CSAT.MSG + '\n' + `[${CONSTANT_VALUES.MODULE.CSAT.YES.toString()}](${yesCsatlink})` + '\n' +
+                `[${CONSTANT_VALUES.MODULE.CSAT.NO.toString()}](${noCsatlink})` + '\n';
             let issueNumber = context.issue.number ?? context.payload.issue.number;
             await github.rest.issues.createComment({
                 issue_number: issueNumber,
