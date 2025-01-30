262a8fed64828c256d14e1ecff8abb1abd1f1529
balanprasanth <112931254+balanprasanth@users.noreply.github.com>
2025-01-30 23:01:50 +0530

    Update constant.js
    Fix: CSAT issue

--- .github/workflows/scripts/constant.js
+++ .github/workflows/scripts/constant.js
@@ -36,7 +36,7 @@ let CONSTANT_VALUES = {
             'https://docs.google.com/forms/d/e/1FAIpQLSfaP12TRhd9xSxjXZjcZFNXPG' +
             'k4kc1-qMdv3gc6bEP90vY1ew/viewform',
         MEDIA_PIPE_BASE_URL:
-            'https://docs.google.com/forms/d/e/1FAIpQLScOLT8zeBHummIZFnfr9wqvxYzWD1DAypyvNia5WVIWtFANYg/viewform?',
+            'https://docs.google.com/forms/d/e/1FAIpQLScOLT8zeBHummIZFnfr9wqvxYzWD1DAypyvNia5WVIWtFANYg/viewform',
         SATISFACTION_PARAM: 'entry.85265664',
         ISSUEID_PARAM: 'entry.2137816233',
         MSG: 'Are you satisfied with the resolution of your issue?',
